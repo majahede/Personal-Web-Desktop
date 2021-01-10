b7d0723d7b4fbec763787cf18084a9b4ad09b10c
Maja Hedegard <mh223pi@student.lnu.se>
2021-01-10 12:41:53 +0100

    add appContainer.js

---
+++ src/components/appContainer/appContainer.js
@@ -0,0 +1,34 @@
+/**
+ * The app container web component module.
+ *
+ * @author Maja Hedegärd <mh223pi@student.lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Define template.
+ */
+const template = document.createElement('template')
+template.innerHTML = `
+
+`
+/**
+ * Define custom element.
+ */
+customElements.define('app-container',
+  /**
+   * @class
+   */
+  class extends HTMLElement {
+    /**
+     * Creates an instance of the current type.
+     */
+    constructor () {
+      super()
+      // Attach a shadow DOM tree to this element and
+      // append the template to the shadow root.
+      this.attachShadow({ mode: 'open' })
+      this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+  }
+)
