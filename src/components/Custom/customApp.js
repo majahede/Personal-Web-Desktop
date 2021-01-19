2ba17f7f9c3fb61b3f9c53f1f87e55507513660e
Maja Hedegard <mh223pi@student.lnu.se>
2021-01-19 12:44:56 +0100

    add convertCurrency

--- src/components/Custom/customApp.js
+++ src/components/Custom/customApp.js
@@ -13,13 +13,20 @@ template.innerHTML = `
 
 
 <div class="container">
-  <div class="currency">
+  <form id="currency">
+  <div class="from">
     <select id="from-currency">
       <option>Convert from</option>
     </select>
+    <input type="text" id="amount"/>
+  </div>
+  <div>
     <select id="to-currency">
       <option>Convert to</option>
     </select>
+    <output></output>
+  </div>
+</form>
 </div>
 `
 /**
@@ -41,6 +48,7 @@ customElements.define('custom-app',
       this.shadowRoot.appendChild(template.content.cloneNode(true))
       this.fromCurrency = this.shadowRoot.querySelector('#from-currency')
       this.toCurrency = this.shadowRoot.querySelector('#to-currency')
+      this.amount = this.shadowRoot.querySelector('#amount')
     }
 
     /**
@@ -48,18 +56,29 @@ customElements.define('custom-app',
      */
     connectedCallback () {
       this.addCurrency()
-      this.fromCurrency.addEventListener('change', (event)=> {
-        console.log(event.target.value)
+
+      this.fromCurrency.addEventListener('change', async (event) => {
+        console.log(await this.convertCurrency())
+        console.log(await this.getCurrency())
       })
     }
 
-    async getCurrency() {
+    /**
+     * Gets current currency rates.
+     *
+     * @returns {object} - the currency rates.
+     */
+    async getCurrency () {
       const URL = 'https://api.exchangeratesapi.io/latest'
       const getCurrency = await window.fetch(URL)
       const currency = await getCurrency.json()
+      currency.rates.EUR = 1
       return currency.rates
     }
 
+    /**
+     * Adds currency options.
+     */
     async addCurrency () {
       const currency = await this.getCurrency()
       const sortedCurrency = Object.keys(currency).sort().reduce(
@@ -69,7 +88,6 @@ customElements.define('custom-app',
         },
         {}
       )
-
       for (const key in sortedCurrency) {
         const option = document.createElement('option')
         option.value = key
@@ -78,6 +96,29 @@ customElements.define('custom-app',
         this.fromCurrency.appendChild(option)
         this.toCurrency.appendChild(clone)
       }
+    }
+
+    /**
+     * Converts one currenecy to another.
+     *
+     * @returns {number} - the converted currency.
+     */
+    async convertCurrency () {
+      const currency = await this.getCurrency()
+      let firstCurrency = 0
+      let secondCurrency = 0
+      const amount = this.amount.value
+
+      for (const key in currency) {
+        if (key === this.fromCurrency.value) {
+          firstCurrency = currency[key]
+        }
+        if (key === this.toCurrency.value) {
+          secondCurrency = currency[key]
+        }
+      }
+      const result = (currency.EUR / firstCurrency) * secondCurrency * amount
+      return result.toFixed(2)
     }
   }
 )
